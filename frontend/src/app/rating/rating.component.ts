c22fcaa3b69d52a6b3bbacdb70a8943bca2a7d69
RayanPrevost <rayan.prvpro@gmail.com>
2022-11-15 15:31:48 +0100

    Mise en place du formulaire de notation des chaussures
    Mise en place du formulaire de notation des chaussures

--- frontend/src/app/rating/rating.component.ts
+++ frontend/src/app/rating/rating.component.ts
@@ -1,16 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { ShoeService } from "../shared/shoe.service";
-
+import { RateService } from "../shared/rate.service";
+import { AuthService } from "../shared/auth.service";
 
 @Component({
   selector: 'app-rating',
   templateUrl: './rating.component.html',
-  styleUrls: ['./rating.component.css']
+  styleUrls: ['./rating.component.css'],
+  providers: [RateService]
 })
 export class RatingComponent implements OnInit {
 
-  constructor(public shoeService:ShoeService) { }
+  constructor(public shoeService:ShoeService, public rateService:RateService, public authService:AuthService) { }
 
   ngOnInit(): void {
     this.getShoeInformation();
@@ -26,6 +28,32 @@ export class RatingComponent implements OnInit {
     })
   }
 
+  onSubmit(form?: NgForm){
+    let shoe_id = (<HTMLInputElement>document.getElementById("shoe_id")).value;
+    let user_id = sessionStorage.getItem("id");
+    let amortiGrade = document.getElementById('amortiGrade')?.attributes[20].value;
+    let confortGrade = document.getElementById('confortGrade')?.attributes[20].value;;
+    let durabiliteGrade = document.getElementById('durabiliteGrade')?.attributes[20].value;;
+    let designGrade = document.getElementById('designGrade')?.attributes[20].value;;
+    let maintienGrade = document.getElementById('maintienGrade')?.attributes[20].value;;
+    let gripGrade = document.getElementById('gripGrade')?.attributes[20].value;;
+    
+    let rateObject = {
+      shoe_id : shoe_id,
+      user_id : user_id,
+      amortiGrade: amortiGrade,
+      confortGrade: confortGrade,
+      durabiliteGrade: durabiliteGrade,
+      designGrade: designGrade,
+      maintienGrade: maintienGrade,
+      gripGrade: gripGrade
+    };
+
+    this.rateService.postRate(rateObject).subscribe((response:any)=>{
+      console.log(response);
+    })
+    
+  }
   
 
 
---
+++ frontend/src/app/shared/rate.ts
@@ -0,0 +1,11 @@
+export class Rate {
+    _id?:String;
+    shoe_id? : String;
+    user_id? : String;
+    amortiGrade? : Number;
+    confortGrade? : Number;
+    durabiliteGrade? : Number;
+    designGrade? : Number;
+    maintienGrade? : Number;
+    gripGrade? : Number;
+}
