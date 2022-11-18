67cacc17159271783314447eb4e41601fcc27a51
RayanPrevost <rayan.prvpro@gmail.com>
2022-11-18 15:43:11 +0100

    Commit Rate/brand sort
    Good Commit Rate/brand sort

--- frontend/src/app/shoes-page/shoes-page.component.ts
+++ frontend/src/app/shoes-page/shoes-page.component.ts
@@ -21,4 +21,28 @@ export class ShoesPageComponent implements OnInit {
     });
   }
 
+  sortName(){
+    console.log(this.shoeService.shoes);
+    this.shoeService.shoes.sort(function compare(a, b) {
+      if (a.shoeName < b.shoeName)
+         return -1;
+      if (a.shoeName > b.shoeName)
+         return 1;
+      return 0;
+      
+    }
+    );console.log(this.shoeService.shoes);
+}
+sortBrand(){
+  console.log(this.shoeService.shoes);
+  this.shoeService.shoes.sort(function compare(a, b) {
+    if (a.brandName < b.brandName)
+       return -1;
+    if (a.brandName > b.brandName)
+       return 1;
+    return 0;
+  }
+  );console.log(this.shoeService.shoes);
+}
+
 }
