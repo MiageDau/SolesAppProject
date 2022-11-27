1facb9beeda5594b8de9d521b66a62dc010dc11d
Dan <dantouboul@outlook.fr>
2022-11-27 13:21:43 +0100

    404 page done and footer only on certain pages

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -26,7 +26,7 @@ const routes: Routes = [
   {path:"login", component:LoginComponent},
   {path:"register", component:RegisterComponent},
   {path:"dashboard", component:AdminDashboardComponent},
-  { path: '**', pathMatch: 'full',component: PagenotfoundComponent },
+  {path: '**', pathMatch: 'full',component: PagenotfoundComponent },
 ];
 
 @NgModule({
--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  constructor(private router: Router){}
   title = 'SolesApp';
+  isHome() {
+    return this.router.url === '/home';
+  }
+  isProfil() {
+    return this.router.url === '/profil';
+  }
+  isInShoes() {
+    return this.router.url === '/shoes';
+  }
 }
+
